0a223ab649796705b969e5300964a5497c08663b
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-28 14:37:48 +0500

    Create main4.dart
    Even/ result int and bool declaration, initialation conditions

---
+++ main4.dart
@@ -0,0 +1,14 @@
+void main() {
+  int numberOne = 10;
+  int numberTwo = 20;
+                                // Even/ result int and bool declaration, initialation conditions
+  bool result = true;
+  bool isEven = false;
+  
+  result = numberTwo < numberOne;
+  
+  isEven = numberTwo / numberOne == 0;
+  
+  print(result);
+  print(isEven);
+}
