1c447d9ba09a9256aa2cacf2d90d3f8be756004f
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-28 14:10:16 +0500

    Create main3.dart
    another example to finds result is true or false

---
+++ main3.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  int numberOne = 10;
+  int numberTwo = 20;
+  
+ 
+  bool results = true;
+  
+    //another example to finds result is true or false
+  print(numberTwo / 2 == 10); 
+  print(numberTwo / 2 == 0);
+  print(numberOne / 2 == 1);
+}
