ed6b476db3d0f6aae0ed8b5aac1f4b64c3b619f8
Muhammad Hashim Javed <53869097+MHashimJaved@users.noreply.github.com>
2023-02-28 13:29:31 +0500

    Create main2.dart
    //  Greater then less then concept in int / boolean:

---
+++ main2.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  int numberOne = 10;
+  int numberTwo = 20;
+                              //  Greater then less then concept in int / boolean:
+ 
+  bool results = true;
+  
+    
+ 
+  print(numberOne < numberTwo);
+  print(numberOne > numberTwo);
+}
